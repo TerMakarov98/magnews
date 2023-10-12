5c8d3a100c3af6718599214392c23c3b38e3eea9
Karen Ter-Makarov <karentermakarov@gmail.com>
2023-10-12 22:22:20 +0400

    ready front

---
+++ admin/includes/footer.php
@@ -0,0 +1,62 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://multinity.com/">Multinity</a>
+    </div>
+    <div class="footer-right"></div>
+</footer>
+</div>
+</div>
+
+<script src="../../admin/dist/modules/jquery.min.js"></script>
+<script src="../../admin/dist/modules/popper.js"></script>
+<script src="../../admin/dist/modules/tooltip.js"></script>
+<script src="../../admin/dist/modules/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../admin/dist/modules/nicescroll/jquery.nicescroll.min.js"></script>
+<script src="../../admin/dist/modules/scroll-up-bar/../../admin/dist/scroll-up-bar.min.js"></script>
+<script src="../../admin/dist/js/sa-functions.js"></script>
+
+<script src="../../admin/dist/modules/chart.min.js"></script>
+<script src="../../admin/dist/modules/summernote/summernote-lite.js"></script>
+
+<script>
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            datasets: [{
+                label: 'Statistics',
+                data: [460, 458, 330, 502, 430, 610, 488],
+                borderWidth: 2,
+                backgroundColor: 'rgb(87,75,144)',
+                borderColor: 'rgb(87,75,144)',
+                borderWidth: 2.5,
+                pointBackgroundColor: '#ffffff',
+                pointRadius: 4
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 150
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            },
+        }
+    });
+</script>
+<script src="../../admin/dist/js/scripts.js"></script>
+<script src="../../admin/dist/js/custom.js"></script>
+<script src="../../admin/dist/js/demo.js"></script>
+</body>
+</html>
---
+++ admin/includes/navbar.php
@@ -0,0 +1,35 @@
+<div id="app">
+    <div class="main-wrapper">
+        <div class="navbar-bg"></div>
+        <nav class="navbar navbar-expand-lg main-navbar">
+            <form class="form-inline mr-auto">
+                <ul class="navbar-nav mr-3">
+                    <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="ion ion-navicon-round"></i></a></li>
+                    <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="ion ion-search"></i></a></li>
+                </ul>
+                <div class="search-element">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn" type="submit"><i class="ion ion-search"></i></button>
+                </div>
+            </form>
+            <ul class="navbar-nav navbar-right">
+                <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="ion ion-ios-bell-outline"></i></a>
+                    <div class="dropdown-menu dropdown-list dropdown-menu-right">
+                        <div class="dropdown-header">Notifications
+                            <div class="float-right">
+                                <a href="#">View All</a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg">
+                        <i class="ion ion-android-person d-lg-none"></i>
+                        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a href="#" class="dropdown-item has-icon">
+                            <i class="ion ion-log-out"></i> Logout
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
